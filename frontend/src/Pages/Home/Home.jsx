6f1cf2197d8b0be1d034b21e1723f155b2cd403d
Somalita <somalitapriyadarshinee0307@gmail.com>
2025-09-10 11:53:42 +0530

    Initial commit

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            Somalita Url Shortener
           </Title>
 
           <Text
